4659d21390a8385c95fd91f44d151af0496755d3
Arthur Felipe <92448606+arthurfaraujo@users.noreply.github.com>
2024-11-23 02:04:46 +0000

    funcionando

--- baralho/baralho.py
+++ baralho/baralho.py
@@ -12,6 +12,44 @@ class Card:
 
     def __str__(self) -> str:
         return f'{self.figure} of {self.suit}'
+
+    def __repr__(self) -> str:
+        return f'\'{self.figure} of {self.suit}\''
+
+    def __gt__(self, other: 'Card') -> bool:
+        return self.value > other.value
+
+    def __lt__(self, other: 'Card') -> bool:
+        return self.value < other.value
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Card):
+            return NotImplemented
+        return self.value == other.value
+    
+class Player:
+    
+    def __init__(self, name: str, cards: list[Card] | None = None):
+        if cards is None:
+            cards = []
+
+        self.name = name
+        self.cards = cards
+
+    def play(self) -> tuple['Player', Card]:
+        return (self, self.cards.pop(0))
+    
+    def receive(self, cards: list[Card]) -> None:
+        self.cards += cards
+
+    def get_total_cards(self) -> int:
+        return len(self.cards)
+
+    def __str__(self) -> str:
+        return str({'name': self.name, 'cards': self.cards})
+
+    def __repr__(self) -> str:
+        return str({'name': self.name, 'cards': self.cards})
     
 class Deck:
     
@@ -24,18 +62,22 @@ class Deck:
     def shuffle(self) -> None:
         random.shuffle(self.cards)
 
-    def distribute(self, players: int) -> list[list[Card]]:
-        quantity = len(self.cards) // players
+    def distribute(self, players: list[Player]) -> None:
+        quantity = len(self.cards) // len(players)
         
         self.shuffle()
 
-        decks: list[list[Card]] = [[] * players]
+        decks: list[list[Card]] = []
 
-        for _ in range(quantity):
-            for player in range(players):
-                decks[player].append(self.cards.pop())
+        for i in range(len(players)):
+          decks.append([])
+          for _ in range(quantity):     
+            decks[i].append(self.cards.pop())
 
-        return decks
+        for player in players:
+            deck = decks.pop()
+
+            player.receive(deck)
     
     def __str__(self) -> str:
         string = ''
@@ -44,37 +86,70 @@ class Deck:
             string += card.__str__() + '\n'
 
         return string
-
-class Player:
-    
-    def __init__(self, name: str, cards: list[Card]):
-        self.name = name
-        self.cards = cards
-
-    def play(self) -> Card:
-        return self.cards.pop(0)
-    
-    def receive(self, cards: list[Card]) -> None:
-        for card in cards:
-          self.cards.append(card)
             
 class Game:
     
-    def __init__(self, players: list[Player], deck: Deck):
-        self.players = players
-        self.deck = deck
+    def __init__(self, players_names: list[str]):
+        self.deck = Deck()
+        self.players = [Player(name) for name in players_names]
+        self.deck.distribute(self.players)
+        self.draw_cards: list[Card] = []
+        self.rounds = 0
 
-    def give_cards(self) -> None:
-        hands = deck.distribute(len(self.players))
+    def round(self) -> None:
+        plays: list[tuple[Player, Card]] = []
+        self.rounds += 1
         
+        print(f'Round {self.rounds}\n')
+        self.show_player_total_cards()
+        print()
+
         for player in self.players:
-            player.cards = hands.pop()
+            play = player.play()
+            plays.append(play)
+            print(f'{player.name} played {play[1]}')
+
+        print()
+        
+        winner = plays[-1]
+        draw = False
+        
+        for idx in range(len(plays) - 1):
+            if plays[idx][1] > winner[1]:
+                winner = play
+            elif plays[idx][1] == winner[1]:
+                draw = True
+        
+        if draw:
+            for _, card in plays:
+              self.draw_cards.append(card)
+
+            print('Draw!\n')
+        else:
+            print(f'{winner[0].name} won the round with {winner[1]}\n')
+            winner[0].receive([card for _, card in plays])
+            winner[0].receive([card for card in self.draw_cards])
+            self.draw_cards = []
+        
+        self.show_player_total_cards()
+
+        for player in self.players:
+            if not len(player.cards):
+                self.players.remove(player)
+        
+        input('\nPress Enter to continue...\n')
+
+    def start(self) -> None:
+        print('Game started!\n')
+        while len(self.players) > 1:
+            self.round()
+        
+        print(f'{self.players[0].name} won the game!')
+
+    def show_player_total_cards(self) -> None:
+        for player in self.players:
+            print(f'{player.name} has {player.get_total_cards()} cards')
 
 if __name__ == '__main__':
-    deck = Deck()
-    print(deck)
-    decks = deck.distribute(4)
-
-    for player, hand in enumerate(decks):
-        print(f'player {player + 1}: {[str(card) for card in hand]}')
-
+    game = Game(['Player 1', 'Player 2'])
+    game.start()
